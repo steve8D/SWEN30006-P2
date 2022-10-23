35ca3880b9646acd3f0a4519d3a089031a2a803d
Danny Ngo <70412140+steve8D@users.noreply.github.com>
2022-10-23 19:18:27 +1100

    Removed more unused variables

--- src/thrones/game/utility/rules/LegalityChecker.java
+++ src/thrones/game/utility/rules/LegalityChecker.java
@@ -4,8 +4,5 @@ import ch.aplu.jcardgame.Card;
 import thrones.game.character.Character;
 
 public abstract class LegalityChecker {
-    protected static final String HEARTTURN = "HEART";
-    protected static final String EFFECTTURN = "EFFECT";
-
     public abstract boolean isLegal(Character targetCharacter, Card attemptedCard);
 }
